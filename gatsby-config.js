c5efe7b5a9859178175cf70b7ed50c5cf1443553
eroub <evan.roub@ergonomyx.com>
2020-01-27 19:08:13 -0800

    titles are important

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio`,
+    description: `Writing history as I go.`,
+    author: `Evan Roubekas`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
