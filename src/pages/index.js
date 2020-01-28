0bfb86069133cb5fa2b7ba4be5a026d2f3e0d413
eroub <evan.roub@ergonomyx.com>
2020-01-27 19:05:36 -0800

    circular linking

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,20 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Portfolio" />
+    <h1>... welcome</h1>
+    <p>See what I've been up to.</p>
+    <a href="https://sol.roubekas.com">
+      SolData
+    </a>
+    <br/>
+    <a href="https://blog.roubekas.com">
+      Blog
+    </a>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
